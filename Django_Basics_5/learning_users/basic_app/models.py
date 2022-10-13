b8e456bf631eec9400976829a30fa3ed5e8b2559
Gollakota Madhuri <madhuri.gollakota@tigeranalytics.com>
2022-10-13 22:11:02 +0530

    Django Level 5 exercises

---
+++ Django_Basics_5/learning_users/basic_app/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_pic = models.ImageField(upload_to="profile_pics/", blank=True)
+    portfolio_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
